af356cd434b6e8abd4864ebedad57cac529333bb
Musa Sartev <47783135+Inspiratie@users.noreply.github.com>
2021-11-06 14:11:52 +0200

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,18 @@
+let myStr = 0;
+function clickOnEq() {
+   document.getElementById('calculate-area').value = myStr;
+}
+
+myStr = 0;
+
+function clickOn7() {
+   myStr += 7;
+   document.getElementById('calculate-area').value = 7;
+}
+
+document.getElementById('calculate-area').value = myStr;
+
+function clickOn8() {
+   myStr += 8;
+}
+
